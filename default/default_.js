66d3f63a245d646782960c45a83670efc3b39eba
UnbiasedBrigade <48119401+UnbiasedBrigade@users.noreply.github.com>
2019-04-16 18:18:06 +0530

    I added code!

--- default/default_.js
+++ default/default_.js
@@ -1,1 +1,8 @@
-//I will add some code later
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
